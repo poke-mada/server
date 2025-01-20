a77b930d0a51c46735aa08c58c388ebafc8a1a91
Alfredo Martinez <xshaffter@gmail.com>
2025-01-20 00:59:16 -0600

    pk-7

--- api/views.py
+++ api/views.py
@@ -163,15 +163,15 @@ def box_saver(boxes, trainer):
     for box_num, data in boxes.items():
         box = TrainerBox.objects.get(box_number=box_num, trainer=trainer)
         for slot in data:
-            slot, _ = TrainerBoxSlot.objects.get_or_create(box=box, slot=slot['slot'])
+            box_slot, _ = TrainerBoxSlot.objects.get_or_create(box=box, slot=slot['slot'])
             if slot['pokemon']:
                 pokemon_serializer = TrainerPokemonSerializer(data=slot['pokemon'])
                 if pokemon_serializer.is_valid(raise_exception=True):
                     pokemon = pokemon_serializer.save()
-                    slot.pokemon = pokemon
+                    box_slot.pokemon = pokemon
             else:
-                slot.pokemon = None
-            slot.save()
+                box_slot.pokemon = None
+            box_slot.save()
 
     return False
 
