4566d5487222619bf8102b4f69f824f60701344e
Alfredo Martinez <xshaffter@gmail.com>
2025-01-22 01:49:36 -0600

    PKMN-6 dynamic height for speed table

--- api/views.py
+++ api/views.py
@@ -199,7 +199,7 @@ class WildcardViewSet(viewsets.ReadOnlyModelViewSet):
             if buyed and used:
                 return Response(data=dict(detail='card_bought_and_used'), status=status.HTTP_200_OK)
             return Response(data=dict(detail='contact_paramada'), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        return Response(data=dict(detail='no_enough_money'), status=status.HTTP_200_OK)
+        return Response(data=dict(detail='no_enough_money'), status=status.HTTP_400_BAD_REQUEST)
 
 
 def team_saver(team, trainer):
