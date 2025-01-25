5f9bfc7063bc77695bd6048ecf47a76aa564f8f7
Alfredo Martinez <xshaffter@gmail.com>
2025-01-25 05:08:32 -0600

    PKMN-6 dynamic height for speed table

--- api/views.py
+++ api/views.py
@@ -236,6 +236,8 @@ def team_saver(team, trainer):
     for pokemon in team:
         if not trainer.current_team:
             continue
+        if not pokemon:
+            continue
         last_version = trainer.current_team.team.filter(mote=pokemon['mote']).first()
         if last_version:
             pokemon['notes'] = last_version.notes
