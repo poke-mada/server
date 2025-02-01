0df709fea5b7a1b6f354e02c6ac8040822b95bc1
Alfredo Martinez <xshaffter@gmail.com>
2025-02-01 03:48:40 -0600

    suffix for base form

--- pokemada/base_settings.py
+++ pokemada/base_settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATICFILES_STORAGE = '.storage.WhiteNoiseStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
