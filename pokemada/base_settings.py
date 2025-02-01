94b108d9406d5e5ce73e3c31bdc5684c86a0edee
Alfredo Martinez <xshaffter@gmail.com>
2025-02-01 04:17:25 -0600

    suffix for base form

--- pokemada/base_settings.py
+++ pokemada/base_settings.py
@@ -112,7 +112,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_STORAGE = '.storage.WhiteNoiseStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
@@ -136,10 +135,10 @@ REST_FRAMEWORK = {
 
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "pokemada.storage.WhiteNoiseStaticFilesStorage",
     },
     "default": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "pokemada.storage.WhiteNoiseStaticFilesStorage",
     }
 }
 DATABASES = {
