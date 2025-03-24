bfe0cea99ff1957d9a8538a571c0a3a6e441f8fe
Alfredo Martinez <xshaffter@gmail.com>
2025-03-24 03:29:17 -0600

    trained pokemon reward

--- event_api/models.py
+++ event_api/models.py
@@ -171,7 +171,7 @@ class StreamerWildcardInventoryItem(models.Model):
 
 
 class Streamer(models.Model):
-    trainer = models.OneToOneField(Trainer, on_delete=models.PROTECT, related_name="streamer")
+    trainer = models.OneToOneField(Trainer, on_delete=models.PROTECT, related_name="streamer", null=True)
     rewards = models.ManyToManyField(RewardBundle)
     name = models.CharField(max_length=50, default="")
 
