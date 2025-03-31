756ddfafc815000879cec0b86d40005210fb6046
Alfredo Martinez <xshaffter@gmail.com>
2025-03-31 15:07:23 -0600

    DJ-fix awawawa

--- api/permissions.py
+++ api/permissions.py
@@ -10,7 +10,11 @@ class IsRoot(BasePermission):
 class IsTrainer(BasePermission):
     def has_permission(self, request, view):
         user: User = request.user
-        return request.user.is_superuser or hasattr(user, 'trainer_profile') or hasattr(user, 'coaching_profile')
+        has_coaching = hasattr(user, 'coaching_profile')
+        has_training = hasattr(user, 'trainer_profile')
+        has_sudo = request.user.is_superuser
+        print(has_coaching, has_training, has_sudo)
+        return has_sudo or has_training or has_coaching
 
 
 class IsCoach(BasePermission):
