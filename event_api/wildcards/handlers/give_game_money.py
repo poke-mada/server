149696c14805971b1f84278eb2e032e61aeefefa
Alfredo Martinez <xshaffter@gmail.com>
2025-05-18 23:02:08 -0600

    give game money handler

--- event_api/wildcards/handlers/give_game_money.py
+++ event_api/wildcards/handlers/give_game_money.py
@@ -1,15 +1,15 @@
-from event_api.wildcards.handlers.settings.models import GiveItemHandlerSettings, GiveMoneyHandlerSettings
+from event_api.wildcards.handlers.settings.models import GiveGameMoneyHandlerSettings
 from event_api.wildcards.registry import WildCardExecutorRegistry
 from event_api.wildcards.wildcard_handler import BaseWildCardHandler
 
 
 @WildCardExecutorRegistry.register("give_game_money", verbose='Give Game Money Handler')
 class GiveGameMoneyHandler(BaseWildCardHandler):
-    admin_inline_model = GiveMoneyHandlerSettings  # a model with extra config
+    admin_inline_model = GiveGameMoneyHandlerSettings  # a model with extra config
 
     def execute(self, context):
         amount = context.get('amount')
-        settings: GiveItemHandlerSettings = self.wildcard.give_item_settings
+        settings: GiveGameMoneyHandlerSettings = self.wildcard.give_game_money_settings
         return {
             'command': "give_money",
             'data': {
