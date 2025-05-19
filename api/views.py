afabcdd64625d67b662f2b382043691dfc5ab4c0
Alfredo Martinez <xshaffter@gmail.com>
2025-05-18 22:57:32 -0600

    give game money handler

--- api/views.py
+++ api/views.py
@@ -312,11 +312,12 @@ class WildcardViewSet(viewsets.ReadOnlyModelViewSet):
     @action(methods=['POST'], detail=True)
     def buy_and_use_card(self, request, *args, **kwargs):
         wildcard: Wildcard = self.get_object()
+        user = request.user
         quantity = int(request.data.get('quantity', 1))
-        if wildcard.can_buy(request.user, quantity):
-            buyed = wildcard.buy(request.user, quantity)
+        if wildcard.can_buy(user, quantity):
+            buyed = wildcard.buy(user, quantity)
             if buyed:
-                used = wildcard.use(request.user, quantity, **request.data)
+                used = wildcard.use(user, quantity, **request.data)
                 if used is True:
                     return Response(data=dict(detail='card_bought_and_used', amount=buyed), status=status.HTTP_200_OK)
                 elif used is False:
