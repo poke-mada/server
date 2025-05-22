6875f51bcd59a845054db422cd7a0d2a4c43bec4
Alfredo Martinez <xshaffter@gmail.com>
2025-05-21 23:50:31 -0600

    websocket asgi

--- pokemada/ws_urls.py
+++ pokemada/ws_urls.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from websocket.sockets import MyConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/alerts/$', MyConsumer.as_asgi()),
+    re_path(r'ws/alerts/(?P<room_name>\w+)$', MyConsumer.as_asgi()),
 ]
