4de85164f0952cfccda438b18ea1d787c61f7ab8
Alfredo Martinez <xshaffter@gmail.com>
2025-05-21 20:49:26 -0600

    ignore caches

--- pokemada/asgi.py
+++ pokemada/asgi.py
@@ -1,18 +1,27 @@
 import os
-import django
-from channels.routing import get_default_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
 from api.router import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemada.settings')
-django.setup()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pokemada.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+
 application = ProtocolTypeRouter({
-    "http": get_default_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
         )
     ),
 })
-sudo systemctl status gunicorn
--- pokemada/base_settings.py
+++ pokemada/base_settings.py
@@ -177,3 +177,12 @@ LOGGING = {
         },
     },
 }
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
