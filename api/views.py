a6101cbae035aaa2b08b7e5846c831e6eecd7e1e
Alfredo Martinez <xshaffter@gmail.com>
2025-05-25 04:26:09 -0600

    static files

--- api/views.py
+++ api/views.py
@@ -235,7 +235,7 @@ class TrainerViewSet(viewsets.ReadOnlyModelViewSet):
 
     @action(methods=['get'], detail=True)
     def box(self, request, pk=None, *args, **kwargs):
-        if pk == 'undefined' or pk == 0:
+        if pk == 'undefined' or pk == 0 or pk == '0':
             return Response(status=status.HTTP_400_BAD_REQUEST)
         trainer = Trainer.objects.get(id=pk)
         box_id = request.query_params.get('box', 0)
