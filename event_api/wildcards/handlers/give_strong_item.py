cd044e95aa6cb64e79e37a68c3dd0bd299334d95
Alfredo Martinez <xshaffter@gmail.com>
2025-05-24 22:45:31 -0600

    static files

---
+++ event_api/wildcards/handlers/give_strong_item.py
@@ -0,0 +1,28 @@
+from event_api.wildcards.registry import WildCardExecutorRegistry
+from event_api.wildcards.wildcard_handler import BaseWildCardHandler
+
+mega_stones = [113, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 656, 657, 658, 659, 660, 661, 662, 663, 664, 665, 666, 667, 668, 669, 670, 671, 672, 673, 674, 675, 676, 677, 678, 679, 680, 681, 682, 683, 684, 685]
+
+
+@WildCardExecutorRegistry.register("mega_item", verbose='Give Mega Stone Handler')
+class GiveMegaItemHandler(BaseWildCardHandler):
+
+    def validate(self, context, **kwargs):
+        item_id = context.get('item_id')
+
+        if item_id not in mega_stones:
+            raise ValueError("non_mega_item")
+
+        return True
+
+    def execute(self, context):
+        item_id = context.get('item_id')
+
+        return {
+            'command': "give_item",
+            'data': {
+                "item_id": item_id,
+                "item_bag": 'items',
+                "quantity": 1
+            }
+        }
