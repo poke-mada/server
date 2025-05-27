c9d5831167c99954d203da98205c52179f8f78ac
Alfredo Martinez <xshaffter@gmail.com>
2025-05-26 19:47:38 -0600

    validate streamer

--- pokemada/views.py
+++ pokemada/views.py
@@ -8,10 +8,12 @@ def coach_overlay(request, streamer_name):
     streamer = Streamer.objects.filter(name=streamer_name, user__masters_profile__profile_type=MastersProfile.TRAINER).first()
     if not streamer:
         raise Http404("Streamer not found")
-    coach: MastersProfile = streamer.user.masters_profile.trainer.users.filter(profile_type=MastersProfile.COACH).first()
+
+    coach: MastersProfile = streamer.user.masters_profile.coaches.filter(user__is_active=True).first()
     coach_name = 'Sin coach'
     if coach:
         coach_name = coach.user.streamer_profile.name
+
     return render(request, 'coach_overlay.html', {
         'coach_name': coach_name,
         'streamer_name': streamer_name
@@ -20,7 +22,7 @@ def coach_overlay(request, streamer_name):
 
 def pro_overlay(request, streamer_name):
     streamer = Streamer.objects.filter(name=streamer_name, user__masters_profile__profile_type=MastersProfile.TRAINER).first()
-    if not streamer:
+    if streamer is None:
         raise Http404("Streamer not found")
     return render(request, 'pro_overlay.html', {
         'streamer_name': streamer_name
