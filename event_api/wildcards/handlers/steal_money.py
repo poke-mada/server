a362dcd3efc86e6b2e8c0b220675c47e59192c41
Alfredo Martinez <xshaffter@gmail.com>
2025-07-07 00:07:20 -0600

    dedsafio: imposter log

---
+++ event_api/wildcards/handlers/steal_money.py
@@ -0,0 +1,31 @@
+from event_api.models import CoinTransaction, MastersProfile
+from event_api.wildcards import AlertHandler
+from event_api.wildcards.handlers.settings.models import GiveMoneyHandlerSettings
+from event_api.wildcards.registry import WildCardExecutorRegistry
+from event_api.wildcards.wildcard_handler import BaseWildCardHandler
+
+
+@WildCardExecutorRegistry.register("steal_money", verbose='Steal Money Handler')
+class GiveMoneyHandler(AlertHandler):
+    admin_inline_model = GiveMoneyHandlerSettings  # a model with extra config
+
+    def execute(self, context):
+        target_id = context.get('target_id')[0]
+        amount = context.get('amount')
+        target_profile: MastersProfile = MastersProfile.objects.get(id=target_id)
+
+        money_quantity = self.wildcard.give_money_settings.quantity
+        CoinTransaction.objects.create(
+            profile=target_profile,
+            amount=money_quantity * amount,
+            TYPE=CoinTransaction.OUTPUT,
+            reason=f'se uso la carta {self.wildcard.name} contra ti {amount} veces'
+        )
+
+        CoinTransaction.objects.create(
+            profile=self.user.masters_profile,
+            amount=money_quantity * amount,
+            TYPE=CoinTransaction.INPUT,
+            reason=f'se uso la carta {self.wildcard.name} {amount} veces'
+        )
+        return super().execute(context)
