81fd32a099fdf112b9e6feeecbbaf934be1fa608
Alfredo Martinez <xshaffter@gmail.com>
2025-07-11 14:19:01 -0600

    dedsa tester filter for profiles

--- event_api/admin.py
+++ event_api/admin.py
@@ -183,7 +183,7 @@ class UserProfileAdmin(NestedModelAdmin, UserAdmin):
         'username',
         'masters_profile__trainer',
         'masters_profile__profile_type',
-        'is_staff',
+        'is_tester',
         'is_active',
         'is_staff'
     )
