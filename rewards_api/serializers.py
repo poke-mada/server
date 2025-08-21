0ef17aa4e56ea99ea14fa68c434a59a0e4bba0a9
Alfredo Martinez <xshaffter@gmail.com>
2025-08-20 18:56:56 -0600

    sistema de ruletas

--- rewards_api/serializers.py
+++ rewards_api/serializers.py
@@ -116,7 +116,7 @@ class RouletteSimpleSerializer(serializers.ModelSerializer):
         if total_prices == 0:
             return None
 
-        probs = obj.prices.values('name').annotate(probability=Round(Count('name') * (100 / total_prices), 2))
+        probs = obj.prices.values('name').annotate(probability=Round(Count('name') * (100 / total_prices), 2)).order_by('-probability')
 
         return probs
 
