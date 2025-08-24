b0d7651f77c9667e009fd70ef69ca1ce83e06652
Alfredo Martinez <xshaffter@gmail.com>
2025-08-23 20:39:44 -0600

    descripciion agregada a la BD

--- pokemada/ws_urls.py
+++ pokemada/ws_urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
-from websocket.sockets import OverlayConsumer, DataConsumer
+from websocket.sockets import OverlayConsumer, DataConsumer, GameDataConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/overlay/(?P<room_name>\w+)$', OverlayConsumer.as_asgi()),
     re_path(r'ws/data/(?P<room_name>\w+)$', DataConsumer.as_asgi()),
+    re_path(r'ws/game_data/(?P<room_name>\w+)$', GameDataConsumer.as_asgi()),
 ]
--- websocket/sockets.py
+++ websocket/sockets.py
@@ -105,7 +105,6 @@ class DataConsumer(AsyncWebsocketConsumer):
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
 
-
     @classmethod
     def send_custom_data(cls, streamer_name, new_message):
         from asgiref.sync import async_to_sync
@@ -116,5 +115,45 @@ class DataConsumer(AsyncWebsocketConsumer):
 
         async_to_sync(channel_layer.group_send)(
             f'data_{streamer_name}',
+            {"type": "chat.message", "message": new_message}
+        )
+
+
+class GameDataConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"game_data_{self.room_name}"
+        # Join room group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    async def receive(self, text_data=None, **kwargs):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat.message", "message": message}
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event["message"]
+        await self.send(text_data=json.dumps({"message": message}))
+
+    @classmethod
+    def send_custom_data(cls, streamer_name, new_message):
+        from asgiref.sync import async_to_sync
+        from channels.layers import get_channel_layer
+
+        channel_layer = get_channel_layer()
+        new_message = json.dumps(new_message)
+
+        async_to_sync(channel_layer.group_send)(
+            f'game_data_{streamer_name}',
             {"type": "chat.message", "message": new_message}
         )
