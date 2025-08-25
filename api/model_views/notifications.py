ac0cf5d6a66bb622e84b8e66cd4ca65275f77959
Alfredo Martinez <xshaffter@gmail.com>
2025-08-25 03:56:24 -0600

    surrogate evolutions

--- api/model_views/notifications.py
+++ api/model_views/notifications.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+from django.utils import timezone
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
@@ -13,6 +14,7 @@ class ProfileNotificationViewSet(viewsets.ModelViewSet):
 
     def list(self, request, *args, **kwargs):
         profile: MastersProfile = request.user.masters_profile
-        queryset = self.get_queryset().filter(profile=profile, created_at=(datetime.now() - timedelta(hours=24))).order_by('-created_at')
+        last_24_hours = (timezone.now() - timedelta(hours=24))
+        queryset = self.get_queryset().filter(profile=profile, created_at=last_24_hours).order_by('-created_at')
         serialized = self.serializer_class(queryset, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
