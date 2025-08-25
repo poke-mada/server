9845d26d812ac24003567c3a4388ca29b61291d5
Alfredo Martinez <xshaffter@gmail.com>
2025-08-25 05:59:51 -0600

    iinfinite karma

--- api/model_views/roulette.py
+++ api/model_views/roulette.py
@@ -38,7 +38,7 @@ class RouletteViewSet(viewsets.ReadOnlyModelViewSet):
         price: RoulettePrice = roulette.spin()
 
         bundle = RewardBundle.objects.create(
-            name=f'Comodin Ganado por {roulette.name}',
+            name=f'{price.name} por {roulette.name}',
             user_created=True,
             sender='Ruleta'
         )
