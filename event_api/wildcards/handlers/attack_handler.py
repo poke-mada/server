9739e29f5479a411fc95230f8e08913add1918c8
Alfredo Martinez <xshaffter@gmail.com>
2025-08-25 05:30:02 -0600

    iinfinite karma

--- event_api/wildcards/handlers/attack_handler.py
+++ event_api/wildcards/handlers/attack_handler.py
@@ -11,8 +11,8 @@ class AttackHandler(AlertHandler):
 
         target_current_segment: MastersSegmentSettings = target_profile.current_segment_settings
 
-        if target_profile.current_segment_settings.segment > self.user.masters_profile.current_segment_settings.segment:
-            return 'No puedes atacar a nadie que este en un tramo mas adelante'
+        if target_profile.current_segment_settings.segment != self.user.masters_profile.current_segment_settings.segment:
+            return 'No puedes atacar a nadie que no este en tu mismo tramo'
 
         if target_current_segment.attacks_received_left < self.wildcard.karma_consumption or target_current_segment.attacks_received_left <= 0:
             return 'Ya no puedes atacar a este objetivo'
