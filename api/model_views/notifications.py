606b7651ff9ecbc9b731d389d47e3affcf128401
Alfredo Martinez <xshaffter@gmail.com>
2025-08-25 04:22:01 -0600

    surrogate evolutions

--- api/model_views/notifications.py
+++ api/model_views/notifications.py
@@ -15,6 +15,6 @@ class ProfileNotificationViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         profile: MastersProfile = request.user.masters_profile
         last_24_hours = (timezone.now() - timedelta(hours=24))
-        queryset = self.get_queryset().filter(profile=profile).order_by('-created_at')
+        queryset = self.get_queryset().filter(profile=profile, created_at__gte=last_24_hours).order_by('-created_at')
         serialized = self.serializer_class(queryset, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
