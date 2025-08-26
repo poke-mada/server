968749da6d279ebdc904c9561d33a6657053e675
Alfredo Martinez <xshaffter@gmail.com>
2025-08-26 16:19:34 -0600

    imposters unique for profile-iimposter

--- event_api/wildcards/handlers/revive_starter_handler.py
+++ event_api/wildcards/handlers/revive_starter_handler.py
@@ -4,7 +4,7 @@ from .. import BaseWildCardHandler
 from ...models import DeathLog, BannedPokemon
 
 
-@WildCardExecutorRegistry.register("revive_pokemon", verbose='Revive Pokemon Handler')
+@WildCardExecutorRegistry.register("revive_starter", verbose='Revive Starter Handler')
 class ReviveStarterHandler(BaseWildCardHandler):
     def execute(self, context):
         last_death = DeathLog.objects.filter(dex_number=self.user.masters_profile.starter_dex_number, profile=self.user.masters_profile, revived=False).first()
