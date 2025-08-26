8663659984584773f1e7c12d08bb598e81ae3024
Alfredo Martinez <xshaffter@gmail.com>
2025-08-26 16:20:51 -0600

    imposters unique for profile-iimposter

--- event_api/wildcards/handlers/revive_starter_handler.py
+++ event_api/wildcards/handlers/revive_starter_handler.py
@@ -6,6 +6,12 @@ from ...models import DeathLog, BannedPokemon
 
 @WildCardExecutorRegistry.register("revive_starter", verbose='Revive Starter Handler')
 class ReviveStarterHandler(BaseWildCardHandler):
+
+    def validate(self, context):
+
+        if not self.user.masters_profile.starter_dex_number:
+            return 'Necesitas decirle al staff cual es tu inicial'
+
     def execute(self, context):
         last_death = DeathLog.objects.filter(dex_number=self.user.masters_profile.starter_dex_number, profile=self.user.masters_profile, revived=False).first()
         last_death.revived = True
