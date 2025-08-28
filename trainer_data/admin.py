f55d02631950927a695c614521c28333a307ba1b
Alfredo Martinez <xshaffter@gmail.com>
2025-08-28 11:20:08 -0600

    a

--- trainer_data/admin.py
+++ trainer_data/admin.py
@@ -70,6 +70,7 @@ class TrainerPokemonAdmin(admin.ModelAdmin):
     readonly_fields = ('moves', 'types', 'suffix', 'enc_data')
     list_display = ('id', 'pokemon__dex_number', 'trainer__name', 'pokemon', 'mote', 'level')
     search_fields = ('pokemon__dex_number', 'pokemon__name', 'mote', 'trainer__name')
+    list_filter = ('trainer__name',)
     inlines = [MoveLinear]
 
 
